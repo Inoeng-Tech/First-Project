85f2402ec49ca64f9ec2f348d033fad78f55dbcf
Big783 <42846810+Big783@users.noreply.github.com>
2018-09-14 14:54:41 +0200

    adding code

--- index_foto.php
+++ index_foto.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 	//connection to Database
 	$con = mysqli_connect("localhost", "web13_usr", "Ueuj114~", "kuebomare");
@@ -23,7 +22,6 @@
 	{
 		echo 'Inserted';
 	}
-	#header("refresh:1; url=index_foto.php");
 	
 	
 	
@@ -44,8 +42,9 @@ $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
     	} else {
         	echo "File is not an image.";
         	$uploadOk = 0;
-    	}
-	}
+		} 
+		
+		}
 	// Check if file already exists
 	if (file_exists($target_file)) {
     	echo "Sorry, file already exists.";
@@ -72,7 +71,9 @@ $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
         	echo "The file ". basename( $_FILES["foto"]["name"]). " has been uploaded.";
     	} else {
         	echo "Sorry, there was an error uploading your file.";
-    	}
+		}
+	
+	
 	}
 					
 
@@ -91,98 +92,4 @@ $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 	
 	
 	
-=======
-<?php
-	//connection to Database
-	$con = mysqli_connect("localhost", "web13_usr", "Ueuj114~", "kuebomare");
-	
-	if(!$con)
-	{
-		echo 'Not cnnected To Server';
-	}
-	
-	$Name = $_POST['name'];
-	$Date = $_POST['date'];
-	$target_file = $_FILES['foto']['name'];
-	$Source = $_POST['source'];
-	
-	$sql = "INSERT INTO foto_DB (name,date,foto,source) VALUES ('$Name','$Date','$target_file','$Source')";
-	
-	if(!mysqli_query($con,$sql))
-	{
-		echo 'Not Inserted';	
-	}
-	else
-	{
-		echo 'Inserted';
-	}
-	#header("refresh:1; url=index_foto.php");
-	
-	
-	
-	//====UPLOAD IMAGE	
-
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["foto"]["name"]);
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-
-	// Check if image file is a actual image or fake image
-
-	if(isset($_POST["submit"])) {
-    	$check = getimagesize($_FILES["foto"]["tmp_name"]);
-    	if($check !== false) {
-    	    echo "File is an image - " . $check["mime"] . ".";
-        	$uploadOk = 1;
-    	} else {
-        	echo "File is not an image.";
-        	$uploadOk = 0;
-    	}
-	}
-	// Check if file already exists
-	if (file_exists($target_file)) {
-    	echo "Sorry, file already exists.";
-    	$uploadOk = 0;
-	}
-	// Check file size
-	if ($_FILES["foto"]["size"] > 500000) {
-    	echo "Sorry, your file is too large.";
-    	$uploadOk = 0;
-	}
-	// Allow certain file formats
-	if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-	&& $imageFileType != "gif" ) {
-    	echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-    	$uploadOk = 0;
-	}
-	// Check if $uploadOk is set to 0 by an error
-	if ($uploadOk == 0) {
-    	echo "Sorry, your file was not uploaded.";
-	
-	// if everything is ok, try to upload file
-	} else {
-    	if (move_uploaded_file($_FILES["foto"]["tmp_name"], $target_file)) {
-        	echo "The file ". basename( $_FILES["foto"]["name"]). " has been uploaded.";
-    	} else {
-        	echo "Sorry, there was an error uploading your file.";
-    	}
-	}
-					
-
-
-?>
-
-
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
->>>>>>> cb783c6f0134ec0e2546b1735ae66ee1e1af79d0
 	
--- insert.php
+++ insert.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 	//connection to Database
 	$con = mysqli_connect("localhost", "web13_usr", "Ueuj114~", "kuebomare");
@@ -24,31 +23,4 @@
 	}
 	header("refresh:1; url=display.php");
 	
-=======
-<?php
-	//connection to Database
-	$con = mysqli_connect("localhost", "web13_usr", "Ueuj114~", "kuebomare");
-	
-	if(!$con)
-	{
-		echo 'Not cnnected To Server';
-	}
-	
-	$Name = $_POST['name'];
-	$Email = $_POST['email'];
-	$Mobile = $_POST['mobile'];
-	
-	$sql = "INSERT INTO test_DB (name,email,mobile) VALUES ('$Name','$Email','$Mobile')";
-	
-	if(!mysqli_query($con,$sql))
-	{
-		echo 'Not Inserted';	
-	}
-	else
-	{
-		echo 'Inserted';
-	}
-	header("refresh:1; url=display.php");
-	
->>>>>>> f46d70d14d843f6dd63e01857677adbdc8ab81eb
 ?>
