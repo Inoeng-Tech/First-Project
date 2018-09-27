8162a67861d5aac3cd2decc082f5a6e2c292c0b6
Big783 <42846810+Big783@users.noreply.github.com>
2018-09-27 15:48:31 +0200

    try

--- insert1.php
+++ insert1.php
@@ -1,1 +1,44 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
 <style type="text/css">
+
+body {background:#CCCCCC; margin:0; padding:0;}
+
+
+
+</style>
+<div>
+
+<form action="" method="post">
+	
+		<fieldset class="first" width="700" height="800">
+        <h1 style="color: #484848;">Information...</h1>
+    	<label class="label1" for="name">Titel* </label>
+    	<input name="titel_name" width="600" height="35"/>
+    
+    	<label for="date">Datum*</label>
+    	<input name="datum_inf" width="600" height="35"/>
+    
+    	<label for="author">Author*</label>
+    	<input name="author_inf" width="600" height="35"/>
+    
+    	<label for="foto">Foto*</label>
+    	<input name="foto_inf" width="600" height="35"/>
+    
+    	<label for="info">Information*</label>
+    	<textarea name="text_info" width="600"></textarea>
+    	</fieldset>
+    
+    	<fieldset>
+    	<input class="btn" name="submit" type="submit" value="Speichern"/>
+    	</fieldset>
+    
+</form>
+
+
+</body>
+</div>
+</html>
