101f63f7fb66b6ac30e9d8913950d43e8325f12c
Big783 <42846810+Big783@users.noreply.github.com>
2020-12-30 20:29:31 +0100

    lowertouppercase.c
    changing font from lowercase to uppercase using function in C

---
+++ lpcase.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+void order(char words[])
+{
+    int i=0;
+
+    for(int i=0; i<strlen(words);i++)
+    {
+        if (words[i]>='a' && words[i]<='z')
+   printf("%c", words[i]-32);
+        else
+   printf("%c", words[i]);
+
+    }
+
+}
+int main()
+{
+    char wordsinput[122], wordsoutput[122];
+    int r;
+
+    scanf("%s", wordsinput);
+
+    order(wordsinput);
+
+    printf("%s\n",wordsoutput);
+
+}
